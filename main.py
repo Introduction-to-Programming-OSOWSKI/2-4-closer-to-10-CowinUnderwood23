646e24e3374da98a28106c411513af50e33c9ee8
CowinUnderwood23 <817281@mystma.org>
2021-09-28 15:05:10 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    
+   
+    
+    if abs(10 - x) > abs(10 - y):
+        return y
+
+    elif abs(10 - y) == abs(10 - x):
+        return 0
+
+    else:
+        return x
+
+print(close10(15, 6))
+
+    
